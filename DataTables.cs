61d0968b96d755637dec47e98552c58f8b776b9b
JYChoi <eos916@gmail.com>
2024-12-13 09:17:07 +0900

    프로젝트 생성

---
+++ DataTables.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class DataTables
+{
+}
+
+public class MonsterData
+{
+    public string monsterID { get; set; }
+    public string name { get; set; }
+    public int hp { get; set; }
+    public int mp { get; set; }
+    public int strength { get; set; }
+    public int intelligence { get; set; }
+    public int vitality { get; set; }
+    public int dexterity { get; set; }
+    public int agility { get; set; }
+    public int luck { get; set; }
+    public int deathExp { get; set; }
+    public int 
+}
